e893bce090f9180ce2825199912750a2336002de
Ashraful Mahin <mahinashraful08@gmail.com>
2025-02-08 14:48:16 -0500

    Testing for Mahin back to backend

---
+++ frontend/app/testback/page.tsx
@@ -0,0 +1,10 @@
+import DataComponent from "@/components/experiment";
+
+export default function TestBack() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold">Test Back</h1>
+      <DataComponent />
+    </div>
+  );
+}
