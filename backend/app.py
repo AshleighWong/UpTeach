0e1c7d35bacb7896f91944786a9835de7d5fa6fc
Evandabest <evanhaque1@gmail.com>
2025-02-08 15:46:22 -0500

    feat: add new function for suggestions

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import os
 import dotenv
-from google import genai
+#from google import genai
 from GetNews import get as get_news
-from flask import jsonify
+import openai
 
 dotenv.load_dotenv()
 
@@ -11,9 +11,43 @@ GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 # Initialize Gemini
 
-client = genai.Client(GEMINI_API_KEY)
+#client = genai.Client(GEMINI_API_KEY)
 
 #model = client.model.generate()
+
+
+client = openai.OpenAI(
+    base_url="https://api.ai.it.cornell.edu", 
+)
+ 
+
+def generate(model, prompt):
+    response = client.chat.completions.create(
+    model=model,
+    messages = [
+        {
+            "role": "user",
+            "content": prompt,
+        }
+        ]
+    )
+    return response['messages'][0]['content']
+
+
+def generate_w_pdfs(model, file_path, prompt):
+    file_obj = openai.Client().files.create(
+        file=open(file_path, "rb"),
+        purpose="assistants"
+    )
+
+    response = openai.Client().chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}],
+        file_ids=[file_obj.id]  
+    )
+
+    return response.choices[0].message['content']
+
 
 
 
@@ -38,6 +72,18 @@ def suggest():
     # Import data from the request from the headers
 
     data = request.headers.get('data')
+
+    file = data.get('file')
+
+    news = get_news()
+
+    prompt = ' Use the following pdf and the following news articles to further enhance and suggest changes to make the lesson plan more relevant and engaging. ' + file + ' ' + news
+
+    response = generate_w_pdfs("", file, prompt)
+
+    return jsonify(response)
+
+    
 
 @app.route('/news')
 def news():
