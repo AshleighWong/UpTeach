64959f13138bc55059cc2045c784c6d9949cb0ff
Evandabest <evanhaque1@gmail.com>
2025-02-08 14:55:08 -0500

    feat: add news route for testing

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+import os
+import dotenv
+from google import genai
+from GetNews import get as get_news
+from flask import jsonify
+
+dotenv.load_dotenv()
+
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
+# Initialize Gemini
+
+client = genai.Client(GEMINI_API_KEY)
+
+#model = client.model.generate()
+
+
+
 
 app = Flask(__name__)
 
@@ -13,6 +31,27 @@ def about():
 @app.route('/mahin')
 def mahin():
     return "Mahin page"
+
+@app.route('/suggest')
+def suggest():
+    
+    # Import data from the request from the headers
+
+    data = request.headers.get('data')
+
+@app.route('/news')
+def news():
+    try:
+        news_data = get_news()
+        return jsonify(news_data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+
+
+
+    
 
 
 if __name__ == '__main__':
